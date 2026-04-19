b94e63a590a63d92e6a8bb3089cc028c3dd09d8f
kenjy007007 <sonyjy83@gmail.com>
2026-04-19 11:56:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-
-from sklearn.utils import _joblib as joblib
+import joblib as joblib
 
 # Configuration visuelle
 st.set_page_config(page_title="IA Salaire Predictor", layout="centered")
